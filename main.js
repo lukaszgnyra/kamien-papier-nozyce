4e10fa3261aef49f497801738f67e27c1fe29ebc
Łukasz Gnyra <lukaszgnyra@outlook.com>
2020-10-03 13:53:45 +0200

    start working on the display

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-const kamien = document.querySelector('.kamien');
-const papier = document.querySelector('.papier');
-const nozyce = document.querySelector('.nozyce');
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
 
-const wynik = document.querySelector('h1 span');
+const score = document.querySelector('.score');
 
 const options = ["Kamień", "Papier", "Nożyce"];
 
 const personSpan = document.querySelector('.person span')
 const computerSpan = document.querySelector('.computer span');
 
-let wyniki = [0,0,0];
+const scoresTable = [0,0,0];
 
-const caseKamien = () => {
+const caseRock = () => {
     personSpan.textContent = "Kamień";
     computerPlay();
     game();
 }
- const casePapier = () => {
+ const casePaper = () => {
     personSpan.textContent = "Papier";
     computerPlay();
     game();
 }
-const caseNozyce = () => {
+const caseScissors = () => {
     personSpan.textContent = "Nożyce";
     computerPlay();
     game();
@@ -35,28 +35,31 @@ const computerPlay = () => {
 }
 
 const game = () => {
-    let p = personSpan.textContent;
-    let c = computerSpan.textContent;
-    if(p == c){
-        wynik.textContent = "remis";
-        wyniki[2]++;
+    let person = personSpan.textContent;
+    let computer = computerSpan.textContent;
+    if(person == computer){
+        score.textContent = "remis";
+        scoresTable[2]++;
     }
-    else if(p=="Kamień" && c=="Nożyce" || p=="Papier" && c=="Kamień" || p=="Nożyce" && c=="Papier"){
-        wynik.textContent="wygrałeś";
-        wyniki[0]++;
+    else if(person=="Kamień" && computer=="Nożyce" || person=="Papier" && computer=="Kamień" || person=="Nożyce" && computer=="Papier"){
+        score.textContent="wygrałeś";
+        scoresTable[0]++;
     }
-    else if(p=="Kamień" && c=="Papier" || p=="Papier" && c=="Nożyce" || p=="Nożyce" && c=="Kamień"){ 
-        wynik.textContent="przegrałeś";
-        wyniki[1]++;
+    else if(person=="Kamień" && computer=="Papier" || person=="Papier" && computer=="Nożyce" || person=="Nożyce" && computer=="Kamień"){ 
+        score.textContent="przegrałeś";
+        scoresTable[1]++;
     }
     tabela();
 }
 
 const tabela = () =>{
-    document.querySelector('.win').textContent= wyniki[0];
-    document.querySelector('.loss').textContent= wyniki[1];
-    document.querySelector('.remis').textContent= wyniki[2];
+    document.querySelector('.win').textContent= scoresTable[0];
+    document.querySelector('.loss').textContent= scoresTable[1];
+    document.querySelector('.remis').textContent= scoresTable[2];
 }
-kamien.addEventListener('click', caseKamien);
-papier.addEventListener('click', casePapier);
-nozyce.addEventListener('click', caseNozyce);
+rock.addEventListener('click', caseRock);
+paper.addEventListener('click', casePaper);
+scissors.addEventListener('click', caseScissors);
+
+
+
